b9691ff2c015f92762ca84cde114fcd32ef771ab
Zsolt Sandor <zsolt.sandor@gmail.com>
2019-04-05 18:59:55 +0200

    constructor is now code free

--- vs-storage/src/main/java/com/acme/videoserver/storage/metadata/Base64EncodedImage.java
+++ vs-storage/src/main/java/com/acme/videoserver/storage/metadata/Base64EncodedImage.java
@@ -4,37 +4,67 @@ import java.util.Base64;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.cactoos.scalar.SolidScalar;
+import org.cactoos.scalar.UncheckedScalar;
+
 import com.acme.videoserver.core.videoclip.Image;
 
 public class Base64EncodedImage implements Image {
 
-	private static final String IMAGE_REGEXP = "data:image\\/([a-zA-Z]*);base64,([^\"]*)";
-	private static final Pattern PATTERN = Pattern.compile(IMAGE_REGEXP);
+	private static final Pattern PATTERN = Pattern.compile("data:image\\/([a-zA-Z]*);base64,([^\"]*)");
 
-	private final byte[] data;
-	private final String mimeType;
+	private final UncheckedScalar<Base64Encoded> output;
 
-	// FIXME make it code free
 	public Base64EncodedImage(String text) {
-		Matcher matcher = PATTERN.matcher(text);
 
-		if (matcher.matches()) {
-			this.mimeType = "image/" + matcher.group(1);
-			this.data = Base64.getMimeDecoder().decode(matcher.group(2));
-		} else {
-			this.mimeType = "";
-			this.data = Base64.getMimeDecoder().decode(text);
-		}
+		this.output = new UncheckedScalar<>(new SolidScalar<>(() -> {
+
+			Matcher matcher = PATTERN.matcher(text);
+
+			String mimeType;
+			byte[] data;
+
+			if (matcher.matches()) {
+				mimeType = "image/" + matcher.group(1);
+				data = Base64.getMimeDecoder().decode(matcher.group(2));
+			} else {
+				mimeType = "";
+				data = Base64.getMimeDecoder().decode(text);
+			}
+
+			return new Base64Encoded(mimeType, data);
+		}));
+
 	}
 
 	@Override
 	public String mimeType() {
-		return mimeType;
+		return output.value().mimeType();
 	}
 
 	@Override
 	public byte[] data() {
-		return data;
+		return output.value().data();
+	}
+
+	private class Base64Encoded {
+
+		private final byte[] data;
+		private final String mimeType;
+
+		public Base64Encoded(String mimeType, byte[] data) {
+			this.mimeType = mimeType;
+			this.data = data;
+		}
+
+		public String mimeType() {
+			return mimeType;
+		}
+
+		public byte[] data() {
+			return data;
+		}
+
 	}
 
 }
